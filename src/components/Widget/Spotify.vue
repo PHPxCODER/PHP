9c63572987740264c53c52e7511b0bc139312608
Subhadeep <codevizag@gmail.com>
2023-12-13 03:48:52 +0530

    Update Lanyard Web Socket

--- src/components/Widget/Spotify.vue
+++ src/components/Widget/Spotify.vue
@@ -153,7 +153,7 @@ export default {
     /**
      * Connect to Lanyard Socket API, send heartbeat every 30 seconds and replace the Vue data value with the message
      */
-    this.socket = new WebSocket('wss://api.codevizag.com/socket')
+    this.socket = new WebSocket('wss://api.phpxcoder.in/socket')
     this.socket.addEventListener('open', () => {
       // Subscribe to ID
       this.socket.send(
